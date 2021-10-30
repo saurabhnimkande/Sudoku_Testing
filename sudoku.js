671bda5eff9c52d8d9d787d7e245175f5461ddaf
SaurabhN16 <91530878+saurabhnimkande@users.noreply.github.com>
2021-10-30 20:19:22 +0530

    sudoku solver without test cases

---
+++ sudoku.js
@@ -0,0 +1,78 @@
+const matrix=[];
+let flag=false;
+function check(board,row,col,val){
+
+  
+    for (let i=0;i<9;i++){
+            if (board[row][i] == val) {
+                return false;
+            }
+        }
+
+  
+    for (let i = 0;i<9;i++){
+            if (board[i][col] == val) {
+                return false;
+            } 
+        }
+
+   
+    let r = Math.floor(row/3)*3;
+    let c = Math.floor(col/3)*3;
+   
+    for (let i=0;i<3;i++){
+        for (let j=0;j<3;j++){
+            if (board[r + i][j + c] == val) {
+                return false;
+            } 
+        }
+    }
+    return true;
+}
+
+function solve(board,x,y){
+    if (x == board.length) { 
+        flag = true;
+        for(let i=0; i<matrix.length; i++) {
+            console.log(matrix[i].join(" "));
+        }
+        return matrix;
+    }
+    let next_row = 0; 
+    let next_col = 0;
+    if (y == board.length-1) { 
+        next_row = x + 1;
+        next_col = 0;
+    } else { 
+        next_row = x;
+        next_col = y + 1;
+    }
+    if (board[x][y] != 0) {
+        solve(board,next_row,next_col); 
+    } else {
+        for (let i=1;i<=9;i++){ 
+            if (check(board,x,y,i)){
+                board[x][y] = i;
+                solve(board,next_row,next_col); 
+                board[x][y] = 0; 
+            }
+        }
+    }
+    return;
+}
+
+function runProgram(input) {
+    // Write code here
+   input=input.trim().split("\n");
+   for(let i=0; i<input.length; i++) {
+       matrix.push(input[i].trim().split(" ").map(Number));
+   }
+   solve(matrix,0,0);
+   return matrix;
+   if(!flag) {
+    return "-1";
+   }
+  }
+
+
+module.exports = runProgram;
